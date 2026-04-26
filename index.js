30f3f7f1847b9e38b406e57ffb184cb22867e5bb
Wang Zuxi <juiceboy327@gmail.com>
2026-04-27 01:31:24 +0800

    perf: replaced onscroll functions with intersection oberservers (more resource friendly and less function calls)

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 
-let scrolledToEdu, scrolledToEduH2, scrolledToEduCards = false
+
+// Animation For Titles and sub-titles
+const observer = new IntersectionObserver(
+    (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+
+            entry.target.style.margin = "3vw 5vw 1vw";
+            entry.target.style.opacity = "1";
+
+            let eh2 = entry.target.nextElementSibling;
+            eh2.style.marginTop = "0";
+            eh2.style.opacity = "1";
+
+        observer.unobserve(entry.target);
+        }
+    });
+    },
+    {
+        threshold: 0.8, // trigger when 30% of element is visible
+    }
+);
+document.querySelectorAll('.title').forEach(e => observer.observe(e));
+
 
 
 document.body.onscroll = ()=>{
@@ -8,32 +31,8 @@ document.body.onscroll = ()=>{
     if (window.scrollY < 30) {
     document.getElementById("navbar").style.marginTop = "-4vw"
     }
-
-    const eduH1 = document.getElementById("eduHeader");
-    const eduH2 = document.getElementById("eduHeader2");
-    const eduCard1 = document.getElementById("card1");
-
-    if (eduH1 && !scrolledToEdu && isInViewport(eduH1)) {
-        scrolledToEdu = true;
-        eduH1.style.margin = "3vw 5vw 1vw";
-    }
-    if (eduH2 && !scrolledToEduH2 && isInViewport(eduH2)) {
-        scrolledToEduH2 = true;
-        eduH2.style.marginTop = "0"
-        eduH2.style.opacity = "1"
-    }
-    if (eduCard1 && !scrolledToEduCards && isInViewport(eduCard1)) {
-        scrolledToEduCards = true;
-        console.log("AYAYAYAYA")
-        document.querySelectorAll(".cards").forEach((e) => {
-            e.style.margin = "3vw 1vw 0vw"
-            e.style.opacity = "1"
-        })
-    }
 }
-
-
-function isInViewport(e) {
-  const rect = e.getBoundingClientRect();
-    return rect.top < window.innerHeight && rect.bottom > 0;
-}
+document.querySelectorAll(".cards").forEach((e) => {
+    e.style.margin = "3vw 1vw 0vw"
+    e.style.opacity = "1"
+})
